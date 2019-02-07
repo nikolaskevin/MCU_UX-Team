54a7e7ff9959cbc9f93779fb227131d9a1ccfc4d
Jordy Then <42790442+jordythen@users.noreply.github.com>
2019-02-07 15:42:21 -0500

    Add files via upload
    Updated 2/7/2019

---
+++ Frontend/Config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAMnEOZj1JB52QOEFPwbS274MX3wQWYCeM",
+  authDomain: "browserteam.firebaseapp.com",
+  databaseURL: "https://browserteam.firebaseio.com",
+  projectId: "browserteam",
+  storageBucket: "browserteam.appspot.com",
+  messagingSenderId: "917274264132"
+ };
+ firebase.initializeApp(config); 
