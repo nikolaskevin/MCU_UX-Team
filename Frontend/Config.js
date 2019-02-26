57399ff955d587262084d2b5bf1b853cc9c7ae8a
Jordy Then <42790442+jordythen@users.noreply.github.com>
2019-02-26 16:20:41 -0500

    remove for update

--- Frontend/Config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  apiKey: "AIzaSyAMnEOZj1JB52QOEFPwbS274MX3wQWYCeM",
-  authDomain: "browserteam.firebaseapp.com",
-  databaseURL: "https://browserteam.firebaseio.com",
-  projectId: "browserteam",
-  storageBucket: "browserteam.appspot.com",
-  messagingSenderId: "917274264132"
- };
- firebase.initializeApp(config); 
