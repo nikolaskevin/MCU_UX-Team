948f9367a6ca73f9827a45eb3c4fdd3304fd87f8
LeonHang <50686942+LeonHang@users.noreply.github.com>
2019-06-03 21:31:15 +0800

    Connect to our own website
    Change the  apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId. So that it can link to our own website

--- Backend-Chinese/Config.js
+++ Backend-Chinese/Config.js
@@ -5,11 +5,11 @@
  * @license
  */
 var config = {
-    apiKey: "AIzaSyAMnEOZj1JB52QOEFPwbS274MX3wQWYCeM",
-    authDomain: "browserteam.firebaseapp.com",
-    databaseURL: "https://browserteam.firebaseio.com",
-    projectId: "browserteam",
-    storageBucket: "browserteam.appspot.com",
-    messagingSenderId: "917274264132"
+ apiKey: "AIzaSyBP1AdMjTWdba-hH_136OPeFODfurH3ENc",
+    authDomain: "mcultc4.firebaseapp.com",
+    databaseURL: "https://mcultc4.firebaseio.com",
+    projectId: "mcultc4",
+    storageBucket: "mcultc4.appspot.com",
+    messagingSenderId: "284533224750"
  };
  firebase.initializeApp(config); 
